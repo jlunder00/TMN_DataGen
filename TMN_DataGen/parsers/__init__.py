30c2512c9f429df06bfaf581cdea9a0f3b74ee2a
Jason Lunder <jlunder@zagmail.gonzaga.edu>
2024-11-24 23:12:22 -0800

    updates

--- TMN_DataGen/parsers/__init__.py
+++ TMN_DataGen/parsers/__init__.py
@@ -1,6 +1,6 @@
 #parsers/__init__.py
 
-from ..utils.logging_config import logger
+from ..utils.logging_config import setup_logger
 from .base_parser import BaseTreeParser
 from .diaparser_impl import DiaParserTreeParser
 from .spacy_impl import SpacyTreeParser
--- TMN_DataGen/tree/__init__.py
+++ TMN_DataGen/tree/__init__.py
@@ -1,4 +1,4 @@
 #__init__.py
-from ..utils.logging_config import logger
+from ..utils.logging_config import setup_logger
 from .node import Node
 from .dependency_tree import DependencyTree
